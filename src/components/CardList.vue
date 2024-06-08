32126e25e90b263d009284c972732b66e33a45a6
Roman Fomichev <romanfomic@gmail.com>
2024-06-08 13:03:27 +0300

    Add filter

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="card-list-container">
+        <div class="filter-section">
+            <input type="text" v-model="filterName" placeholder="Filter by name" class="filter-input"/>
+            <select v-model="filterStatus" class="filter-select">
+                <option value="">All</option>
+                <option value="Alive">Alive</option>
+                <option value="Dead">Dead</option>
+                <option value="Unknown">Unknown</option>
+            </select>
+            <button @click="applyFilters" class="apply-button">Apply</button>
+        </div>
         <div class="card-list">
             <div v-for="characterData in charactersData" :key="characterData.id">
                 <CardItem :character="characterData"/>
@@ -23,19 +33,35 @@
     const charactersData = ref(null);
     const pagesCount = ref(1);
     const currentPage = ref(1);
+    const filterName = ref("");
+    const filterStatus = ref("");
 
+    const applyFilters = async () => {
+        if (filterStatus.value === "All") { filterStatus.value = ""; }
+        getCharacters(1, filterName.value, filterStatus.value)
+    }
     const prevPage = async () => {
         currentPage.value--;
-        getCharacters(currentPage.value);
+        getCharacters(currentPage.value, filterName.value, filterStatus.value);
     }
     const nextPage = async () => {
         currentPage.value++;
-        getCharacters(currentPage.value);
+        getCharacters(currentPage.value, filterName.value, filterStatus.value);
     }
 
-    const getCharacters = async (page) => {
+    const getCharacters = async (page=0, name="", status="") => {
+        let queryString = `${BASE_URL}/character/?`;
+        if(page !== 0) {
+            queryString += `page=${page}`;
+        }
+        if (name !== "") {
+            queryString += `&name=${name}`;
+        }
+        if (status !== "") {
+            queryString += `&status=${status}`;
+        }
         try {
-            const response = await axios.get(`${BASE_URL}/character/?page=${page}`);
+            const response = await axios.get(queryString);
             charactersData.value = response.data.results;
             pagesCount.value = response.data.info.pages;
         } catch (error) {
@@ -54,6 +80,33 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+    .filter-section {
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+    .filter-input, .filter-select, .apply-button {
+        margin-right: 10px;
+    }
+    .filter-input, .filter-select {
+        padding: 8px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        font-size: 16px;
+    }
+    .apply-button {
+        background-color: #34495e;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 8px 16px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+    .apply-button:hover {
+        background-color: #2c3e50;
     }
 
     .card-list-container {
