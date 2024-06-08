d42ceb3c2f16e166de937816534add42f325b0dd
Roman Fomichev <romanfomic@gmail.com>
2024-06-08 11:06:22 +0300

    Add list of cards

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-
+        <div v-for="characterData in charactersData" >
+            <CardItem :character="characterData"/>
+        </div>
     </div>
 </template>
 
 <script setup>
-    import CardItem from '@/components/CardItem.vue'
+    import CardItem from '@/components/CardItem.vue';
+
+    import axios from 'axios';
+    import { BASE_URL } from "../config";
+    import {ref, onMounted} from 'vue';
+
+    const charactersData = ref(null);
+    const currentPage = ref(0);
+
+    const getCharacters = async (page) => {
+        try {
+            const response = await axios.get(`${BASE_URL}/?page=${page}`);
+            charactersData.value = response.data.results;
+        } catch (error) {
+            console.error('The request failed: ', error);
+        }
+    }
+
+    onMounted(() => {
+        getCharacters(0);
+    })
 
 </script>
 
